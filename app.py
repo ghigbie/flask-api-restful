8fbcb22caae857929d352ba1def47fc4a488e4bd
ghigbie <gehigbie@gmail.com>
2019-05-23 19:31:46 -0600

    Edit url for api

--- app.py
+++ app.py
@@ -4,10 +4,20 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
-    def get(self, name):
-        return {'student': name}
+items = []
 
-api.add_resource(Student, '/student/<string:name>')
+class Item(Resource):
+    def get(self, name):
+        for item in items:
+            if name == item.name:
+                return item
+            else:
+                return 'Item does not exist'
+
+    def post(self, name):
+        item = {'name': name, 'price': 12.00}
+        items.append(item)
+
+api.add_resource(Item, '/item/<string:name>')
 
 app.run(port=5000)
