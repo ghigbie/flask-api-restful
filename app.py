243b1df27784318f82a32acef974c33be571fb94
ghigbie <gehigbie@gmail.com>
2019-05-28 21:05:57 -0600

    Add security.py file

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import jwt
 
 app = Flask(__name__)
+app.secret_key = 'secret'
 api = Api(app)
 
 items = []
@@ -9,9 +11,12 @@ items = []
 class Item(Resource):
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name), None)
-        return {'item': item}, 200 if item is not None else 404
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400
+
         data = request.get_json()
         item = {'name': name, 'price': data['price']}
         items.append(item)
